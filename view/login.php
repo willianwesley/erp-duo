23ec49e3ab66a31f802f63e180037561fa3efb83
willianwesley <willianfidelis.duo@gmail.com>
2019-12-19 02:46:31 -0300

    menu opções

--- view/login.php
+++ view/login.php
@@ -4,6 +4,7 @@ session_start();
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
+    <link rel="icon" type="imagem/png" href="../imagens/icone.png"/>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
